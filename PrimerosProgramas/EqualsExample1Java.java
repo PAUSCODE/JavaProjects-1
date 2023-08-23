4d27bd6811748f0ab69155c976f67dec226ae299
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-08-23 09:32:26 -0500

    Add files via upload

---
+++ PrimerosProgramas/EqualsExample1Java.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package equalsexample1.java;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class EqualsExample1Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+
+      System.out.println(line1.equals(line2));
+      System.out.println(line2.equals(line3));
+    }
+    
+}
