c851208561144b66a18c63bd3cbb8d799c341ec1
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-08-23 09:25:57 -0500

    Add files via upload

---
+++ PrimerosProgramas/Char.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgchar;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Char {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char expectedGrade = 'C'; 
+     System.out.println(expectedGrade);
+    }
+    
+}
