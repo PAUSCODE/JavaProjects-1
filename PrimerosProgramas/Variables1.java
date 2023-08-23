839504de0fe77564da6aec4b98d77846e2068198
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-08-23 09:47:24 -0500

    Add files via upload

---
+++ PrimerosProgramas/Variables1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables1;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Variables1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "James Gosling";
+        int yearCreated = 1995;
+        System.out.println(name);
+        System.out.println(yearCreated);
+                
+    }
+    
+}
