a1d829ecd77fa3d95d465de46a7bdee43a56fd3b
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-08-23 09:23:49 -0500

    Add files via upload

---
+++ PrimerosProgramas/Tree.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tree;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Tree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey there, I’m Henry and I’m learning to code in Java!");
+       System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+       System.out.println("   *   ");
+       System.out.println("  ***  ");
+       System.out.println("*******");
+       System.out.println(" *****  ");
+       System.out.println("  ***  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+    }
+    
+}
