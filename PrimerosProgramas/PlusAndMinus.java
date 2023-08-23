08526f5815131e777218fcf73f88867d8a8b9065
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-08-23 09:24:57 -0500

    Add files via upload

---
+++ PrimerosProgramas/PlusAndMinus.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package plusandminus;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class PlusAndMinus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    
+    System.out.println(animalsInZoo); 
+
+    int numZebrasAfterTrade = zebrasInZoo - 2;
+
+    System.out.println(numZebrasAfterTrade);
+
+    }
+    
+}
