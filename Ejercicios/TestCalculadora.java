d134f182e35840eae71587add29905d22fb10b2f
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-09-19 07:38:01 -0500

    Update TestCalculadora.java

--- Ejercicios/TestCalculadora.java
+++ Ejercicios/TestCalculadora.java
@@ -1,9 +1,6 @@
 
 public class TestCalculadora {
 
-    public TestCalculadora ()
-        System.out.println("Soy el constructor de TestCalculadora")
-    
     public static void main (String[]args){
         Calculadora objCalculadora = new Calculadora();
         objCalculadora.operaciones();
