8e41b51d6a7139e942828f350853447d9e5bf1c9
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-09-19 07:07:44 -0500

    Update Usuario.java

--- Ejercicios/Usuario.java
+++ Ejercicios/Usuario.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Usuario {
+    Public Usuario ()
+        System.out.println("Soy el constructor de Usuario")
+    
     public int pedirNumero(){
         Scanner s=new Scanner (System.in);
         System.out.println("Digite un valor ");
