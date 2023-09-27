18820fb91696382ac8f7b82aed6e735bc7cd8525
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-09-27 09:39:15 -0500

    Add files via upload

---
+++ PrimerosProgramas/Person.java
@@ -0,0 +1,27 @@
+
+package test;
+
+public class Person {
+    private String name;
+    private int age;
+    private String favColor;
+    
+    public void setName (String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;       
+    }
+    public void setAge (int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;       
+    }
+    public void setFavColor (String favColor){
+        this.favColor = favColor;
+    }
+    public String getFavColor(){
+        return favColor;       
+    }
+}
