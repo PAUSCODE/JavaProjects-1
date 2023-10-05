309d45a03b0c8e1020525e4e28d774df44d46f80
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-10-05 09:44:08 -0500

    Add files via upload

---
+++ SEGUNDOCORTE/Herencia/TestHerencia.java
@@ -0,0 +1,13 @@
+
+package test;
+
+import herencia.*;
+
+public class TestHerencia {
+    public static void main (String[] args){
+        Persona persona1 = new Persona ("Alex Roel", 'M', 25, "Santo Tomas"); 
+        
+        System.out.println(persona1);
+    }
+    
+}
